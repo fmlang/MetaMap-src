f6a357d3c8498420c46cf38fcc18c8eac4c202bb
Willie Rogers <wjrogers@mail.nih.gov>
2012-07-05 15:28:03 -0400

    Added methods to Java API to expose MetaMap pruning information.

--- javaapi/sources/gov/nih/nlm/nls/metamap/PCM.java
+++ javaapi/sources/gov/nih/nlm/nls/metamap/PCM.java
@@ -16,15 +16,35 @@ public interface PCM extends MetaMapElement {
   /** Return the {@link Phrase} associated with set of Mappings and Candidates.
    * @return the {@link Phrase} instance.
    */
-   Phrase getPhrase() throws Exception ;
-   List<Ev> getCandidates() throws Exception ;
-  /** Return the Candidate ({@link Ev}) list associated with the {@link Phrase}.
-   * @return a List of {@link Ev} candidate instances.
+  Phrase getPhrase() throws Exception ;
+
+  /** Return the ({@link Candidates}) instance associated with the {@link Phrase}.
+   * @return a Candidates instance containing a List of candidate Ev instances.
    */
-   List<Ev> getCandidateList() throws Exception ;
-   List<gov.nih.nlm.nls.metamap.Map> getMappings() throws Exception ;
+  Candidates getCandidatesInstance()  throws Exception ;
+
+  /** Return the candidate ({@link Ev}) list associated with the {@link Phrase}.
+   * @return a List of {@link Ev} candidate Ev instances.
+   * @deprecated
+   */
+  List<Ev> getCandidates() throws Exception ;
+
+  /** Return the Candidate ({@link Ev}) list associated with the {@link Phrase}.
+   * @return a List of {@link Ev} candidate Ev instances.
+
+   */
+  List<Ev> getCandidateList() throws Exception ;
+
+
+  /**
+   * Get list of {@link Mapping} instances associated with {@link Phrase}
+   * @return a List of {@link Map} instances.
+   * @deprecated
+   */
+  List<gov.nih.nlm.nls.metamap.Map> getMappings() throws Exception ;
+
   /** Return the {@link Mapping} list associated with the {@link Phrase}.
    * @return a List of {@link Mapping} instances.
    */
-   List<Mapping> getMappingList() throws Exception ;
+  List<Mapping> getMappingList() throws Exception ;
 }
