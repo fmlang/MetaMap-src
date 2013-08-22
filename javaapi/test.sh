c23eeb14c18ef3046ec51f6a94fbbdc5e0447660
Willie Rogers <wjrogers@mail.nih.gov>
2013-08-22 14:24:29 -0400

    Java API sources have moved to metamap-api repository.
    Entire Java API source history has moved to metamap-api repository.
    
    Java API url is:
    
      /nfsvol/nls/tools/repositories/SKR/metamap-api.git

--- javaapi/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-PBJAR=$QUINTUS/java3.5/prologbeans.jar 
-# java -classpath $PBJAR:./classes TestGUI
-
-java -classpath $PBJAR:./classes gov.nih.nlm.nls.metamap.AutoGen $*
--- javaapi/testgui.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-PBJAR=$QUINTUS/java3.5/prologbeans.jar 
-java -classpath $PBJAR:./build/classes TestGUI
